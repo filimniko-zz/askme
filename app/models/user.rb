c77746c6a6038a61c48a30e670e2c08a379ea375
filimniko <filimniko@gmail.com>
2021-11-21 21:47:18 +0300

    validation of username, email, text

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,48 @@
+require "openssl"
+
 class User < ApplicationRecord
+  ITERATIONS = 20000
+  DIGEST = OpenSSL::Digest::SHA256.new
+
+  has_many :questions
+
+  validates :email, :username, presence: true
+  validates :email, :username, uniqueness: true, on: :create
+  validates :username, length: { maximum: 40 }
+  validates :username, format: { with: /\A[a-zA-Z0-9_]+\z/, message: "only allows letters (\"A-Z\", \"a-z\", \"_\")" }
+
+  validates_each :email do |record, attr, value|
+    record.errors.add(attr, 'email is incorrect') unless value =~ /^[\w\d\.]+@[\w\d]+\.\w/i
+  end
+
+  attr_accessor :password
+
+  validates :password, presence: true
+  validates :password, confirmation: true
+
+  before_save :encrypt_password
+
+  def encrypt_password
+    if self.password.present?
+      # Salt
+      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+
+      #Hash
+      self.password_hash = User.hash_to_string(OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST))
+    end
+  end
+
+  def self.hash_to_string(password_hash)
+    password_hash.unpack("H*")[0]
+  end
+
+  def self.authenticate(email, password)
+    user = find_by(email: email)
+
+    if user.present? and user.password_hash == User.hash_to_string(OpenSSL::PKCS5.pbkdf2_hmac(password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST))
+      user
+    else
+      nil
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_17_101106) do
+ActiveRecord::Schema.define(version: 2021_11_21_171420) do
 
   create_table "questions", force: :cascade do |t|
     t.string "text"
     t.string "answer"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "username"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+# Could not dump table "users" because of following StandardError
+#   Unknown type 'stringrk' for column 'avatar_url'
 
+  add_foreign_key "questions", "users"
 end
